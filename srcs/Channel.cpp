815eb4c7fb32a6b3758044928868850f5831254a
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2024-06-14 17:48:40 +0300

    Add files via upload

---
+++ srcs/Channel.cpp
@@ -0,0 +1,12 @@
+#include "../includes/Server.hpp"
+
+std::vector<int> Channel::getFds() const
+{
+	std::vector<int> file;
+
+	for (std::vector<Client>::const_iterator it = _channelClients.begin(); it != _channelClients.end(); ++it)
+	{
+		file.push_back(it->_cliFd);
+	}
+	return file;
+}
